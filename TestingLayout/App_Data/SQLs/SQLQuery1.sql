581147ff9854025d5c26971156f23074e5da37cb
Martin Svarovsky <msvarovsky@gmail.com>
2017-10-06 17:05:42 +0100

    s

--- TestingLayout/App_Data/SQLs/SQLQuery1.sql
+++ TestingLayout/App_Data/SQLs/SQLQuery1.sql
@@ -1,6 +1,25 @@
 ﻿
 
+ALTER PROCEDURE spGetMenu
+(
+    @MenuCode VARCHAR(50),
+    @LanguageCode VARCHAR(2)
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
 
+	SELECT	sl.Label, mi.URL
+	FROM	Menu m,
+			MenuItems mi,
+			SystemLabels sl
+	WHERE	m.ItemId = mi.Id
+	AND		mi.LabelCode = sl.LabelCode
+	AND		sl.LanguageCode = @LanguageCode
+	AND		m.MenuCode = @MenuCode
+END
+
+EXEC spGetMenu 'home_main', 'en'
 
 sp_helptext spGetLabels
 
